10b510eeddc8e612918bef0e183ce2090cc70127
Alan Pierce <alan@benchling.com>
2018-08-02 07:42:49 -0700

    Address review feedback
    * Plumb errors through.
    * Use shared constants for protocol strings.
    * Call `flattenDiagnosticMessageText`.
    * Resolve file paths to absolute on the server end, just in case.
    * Log instead of throwing on the server side.
    * Bump version to 1.0.2.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import {IPC} from 'node-ipc';
-import {LanguageService} from 'typescript/lib/tsserverlibrary';
+import * as ts_module from "typescript/lib/tsserverlibrary";
 
-import {SimpleDiagnostic} from './Protocol';
+import {CALL_EVENT, SERVER_ID, SimpleDiagnostic} from './Protocol';
+import {resolve} from "path";
 
-function init(): ts.server.PluginModule {
-  function create(info: ts.server.PluginCreateInfo): LanguageService {
+function init(modules: {typescript: typeof ts_module}): ts.server.PluginModule {
+  const ts = modules.typescript;
+  function create(info: ts.server.PluginCreateInfo): ts_module.LanguageService {
     const ipc = new IPC();
-    ipc.config.id = 'ts-expose-status-plugin-server';
+    ipc.config.id = SERVER_ID;
     // We MUST silence logging, or else node-ipc will print to stdout. VSCode talks with the TS
     // language service over stdin/stdout, so any extraneous messages to stdout will break VSCode's
     // normal code intelligence features.
@@ -16,16 +18,17 @@ function init(): ts.server.PluginModule {
       info.project.projectService.logger.info(s);
     }
 
+    function logError(s: string): void {
+      info.project.projectService.logger.msg(s, ts.server.Msg.Err);
+    }
+
     function convertToSimpleDiagnostic(diagnostic: ts.Diagnostic): SimpleDiagnostic {
       return {
-        filePath: diagnostic.file ? diagnostic.file.fileName : null,
+        filePath: diagnostic.file ? resolve(diagnostic.file.fileName) : null,
         start: diagnostic.start != null ? diagnostic.start : null,
         end:
           diagnostic.start != null && diagnostic.length != null ? diagnostic.start + diagnostic.length : null,
-        message:
-          typeof diagnostic.messageText === 'string'
-            ? diagnostic.messageText
-            : diagnostic.messageText.messageText,
+        message: ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'),
         code: diagnostic.code,
       };
     }
@@ -35,6 +38,7 @@ function init(): ts.server.PluginModule {
       if (!program) {
         return [];
       }
+      // Ignore other diagnostic types for now, since they tend to not correspond to typical TS errors.
       const tsDiagnostics = [...program.getSemanticDiagnostics(), ...program.getSyntacticDiagnostics()];
       return tsDiagnostics.map((diagnostic) => convertToSimpleDiagnostic(diagnostic));
     }
@@ -42,6 +46,7 @@ function init(): ts.server.PluginModule {
     function getErrorsForFiles(filenames: Array<string>): Array<SimpleDiagnostic> {
       const diagnostics = [];
       for (const filename of filenames) {
+        // Ignore other diagnostic types for now, since they tend to not correspond to typical TS errors.
         const tsDiagnostics = [
           ...info.languageService.getSemanticDiagnostics(filename),
           ...info.languageService.getSyntacticDiagnostics(filename),
@@ -53,15 +58,21 @@ function init(): ts.server.PluginModule {
 
     log('Starting ts-expose-status-plugin server');
     ipc.serve(() => {
-      ipc.server.on('call', (data, socket) => {
+      ipc.server.on(CALL_EVENT, (data, socket) => {
         log(`Received method call: ${data.method}`);
         let response;
-        if (data.method === 'getAllErrors') {
-          response = getAllErrors();
-        } else if (data.method === 'getErrorsForFiles') {
-          response = getErrorsForFiles(data.filenames);
-        } else {
-          throw new Error(`Unexpected method: ${data.method}`);
+        try {
+          if (data.method === 'getAllErrors') {
+            response = getAllErrors();
+          } else if (data.method === 'getErrorsForFiles') {
+            response = getErrorsForFiles(data.filenames);
+          } else {
+            const errorMsg = `Unexpected method: ${data.method}`;
+            logError(errorMsg);
+            response = errorMsg;
+          }
+        } catch (e) {
+          response = e.message;
         }
         ipc.server.emit(socket, 'respond', response);
       });
