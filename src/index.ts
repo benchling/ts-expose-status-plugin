c008244ff9c5c746a50a512a0444e729dbbd5496
Tyler Gary <tyler.gary@benchling.com>
2025-02-21 16:18:36 -0800

    Finish upgrade

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import {IPC} from 'node-ipc';
-import {appendFileSync} from 'fs';
-import {homedir} from 'os';
+import { appendFileSync } from 'fs';
+import { IPC } from 'node-ipc';
+import { homedir } from 'os';
+import ts from 'typescript';
 import * as ts_module from 'typescript/lib/tsserverlibrary';
 
-import {CALL_EVENT, RESPOND_EVENT, SERVER_ID, SimpleDiagnostic} from './Protocol';
-import {resolve} from 'path';
+import { resolve } from 'path';
+import { CALL_EVENT, RESPOND_EVENT, SERVER_ID, SimpleDiagnostic } from './Protocol';
 
 const LOG_FILE = `${homedir()}/ts-expose-status-plugin-output.log`;
 const LOGGING_ENABLED = false;
@@ -70,7 +71,7 @@ class MultiProjectChecker {
   projectCheckers: Array<ProjectChecker> = [];
 
   registerProject(ts: typeof ts_module, info: ts.server.PluginCreateInfo) {
-    log(`Initializing project ${info.project.projectName}`);
+    log(`Initializing project ${info.project.getProjectName()}`);
     this.projectCheckers.push(new ProjectChecker(ts, info));
   }
 
